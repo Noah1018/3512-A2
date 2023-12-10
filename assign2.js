844d344e8056a7e51d46b435be93524e443c37d7
Dmax98 <mdallas0507@gmail.com>
2023-12-10 00:06:48 -0700

    playlist button working

--- assign2.js
+++ assign2.js
@@ -270,12 +270,15 @@ function createRadarChart(songDetails) {
           pointBorderColor: '#fff',
           pointHoverBackgroundColor: '#fff',
           pointHoverBorderColor: 'rgb(255, 255, 0)',
+          point
         },
       ],
     },
-    
-  });
+  },
+ 
+  );
 }
+
 
 
 function closeSingleSongView() {
@@ -288,15 +291,27 @@ function closeSingleSongView() {
 
 
 // Playlist View >>>>>>>>
+function closeSonglist() {
+  document.querySelector('#songTable').style.display = 'none';
+  
+    document.querySelector('#search').style.display = 'none';
+    document.querySelector('#song-list').style.display = 'none';
+    document.querySelector('#playlist-view').style.display='inline';
+    document.querySelector('#radarChart').style.display = 'none';
+    document.querySelector('#song-details-container').style.display = 'none';
+}
 
 
 let playlist = [];
 
 function addToPlaylist(song) {
+ 
   if (song && song.title) {
       playlist.push(song);
       selectedSong = song; 
+      document.querySelector('#single-song-view').style.display = 'none';
       renderPlaylist();
+      
   } else {
       console.error("Invalid song object:", song);
   }
@@ -361,9 +376,9 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const PlaylistButton = document.querySelector('#PlaylistButton');
-    if (PlaylistButton) {
-      PlaylistButton.addEventListener('click', renderPlaylist);
-    }
+          PlaylistButton.addEventListener('click', closeSonglist);
+
+
 
   const clearButton = document.querySelector('#clearButton');
   if (clearButton) {
